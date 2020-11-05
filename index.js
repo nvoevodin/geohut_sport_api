b4972367174ca87f04d4281b51c7b08d69b93854
verycool <verycool@verycools-MacBook-Pro.local>
2020-11-05 13:13:07 -0500

    ggg

--- index.js
+++ index.js
@@ -1119,7 +1119,7 @@ setInterval(() => {
 
 
 
-            pool.query("Delete FROM geohut_sport.live_courts_info WHERE DATE_ADD(report_datetime, INTERVAL 1 HOUR) < now()", function (err, result) {
+            pool.query("Delete FROM geohut_sport.live_courts_info WHERE DATE_ADD(report_datetime, INTERVAL 2 HOUR) < now()", function (err, result) {
                 console.log('deleted live courts data')
          if (err) throw err;
          console.log("Number of records deleted: " + result.affectedRows);
